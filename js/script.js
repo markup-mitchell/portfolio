1ff01a6257e6e36b75fd041aa791ca7efc4e96b6
spitchell <mr.spitchell@gmail.com>
2017-07-18 14:30:56 +0100

    face animated

--- js/script.js
+++ js/script.js
@@ -1,3 +1,22 @@
 // let landSect = document.querySelector('.landingSection');
 // landSect.style.backgroundImage = 'url("assets/moss_and_lichen.jpg")';
 // landSect.style.backgroundSize = 'cover';
+function draw(pathSelector) {
+  const path = document.querySelector(pathSelector);
+  const pathLength = path.getTotalLength();
+  path.style.transition = path.style.WebkitTransition = 'none';
+  path.style.strokeDasharray = pathLength + ' ' + pathLength; 
+  path.style.strokeDashoffset = pathLength;
+  path.getBoundingClientRect();
+  path.style.transition = path.style.WebkitTransition = 'stroke-dashoffset 2s ease-out';
+  path.style.strokeDashoffset = '0';
+}
+
+let paths = document.querySelectorAll('path');
+
+setTimeout(function(){
+  paths.forEach(function(path) {
+    let pathSelector = `#${path.id}`;
+    draw(pathSelector);
+  }, 10000);
+});
