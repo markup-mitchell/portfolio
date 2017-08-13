b12896e120697c827ec872141e1e9d04be63a144
spitchell <mr.spitchell@gmail.com>
2017-08-13 16:36:07 +0100

    stickyMenu functionality added

--- js/script.js
+++ js/script.js
@@ -55,4 +55,33 @@ function unpulseButtons(){
 setInterval(pulseButtons, 5000);
 setInterval(unpulseButtons, 5600); // these might eventually de-sync?
 
+// STICKY MENU - lifted from https://codepen.io/senff/pen/ayGvD?editors=0110
+// todo - replace with native JS
 
+// Create a clone of the menu, right next to original.
+$('.buttonBox').addClass('original').clone().insertAfter('.buttonBox').addClass('cloned').css('position','fixed').css('top','0').css('margin-top','0').css('z-index','500').removeClass('original').hide();
+
+scrollIntervalID = setInterval(stickIt, 10);
+
+
+function stickIt() {
+
+  var orgElementPos = $('.original').offset();
+  orgElementTop = orgElementPos.top;               
+
+  if ($(window).scrollTop() >= (orgElementTop)) {
+    // scrolled past the original position; now only show the cloned, sticky element.
+
+    // Cloned element should always have same left position and width as original element.     
+    orgElement = $('.original');
+    coordsOrgElement = orgElement.offset();
+    leftOrgElement = coordsOrgElement.left;  
+    widthOrgElement = orgElement.css('width');
+    $('.cloned').css('left',leftOrgElement+'px').css('top',0).css('width',widthOrgElement).show();
+    $('.original').css('visibility','hidden');
+  } else {
+    // not scrolled past the menu; only show the original menu.
+    $('.cloned').hide();
+    $('.original').css('visibility','visible');
+  }
+}
